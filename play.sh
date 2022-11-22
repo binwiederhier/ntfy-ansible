3ac041382785d9b9f23c5ec1b09cff34be063703
Philipp Heckel <pheckel@datto.com>
2022-11-21 21:49:39 -0500

    Logrotate

---
+++ play.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Syntax: $0 [dev|staging|prod]"
+	exit 1
+fi
+
+export env="$1"
+ansible-playbook \
+    -i "inventory/$env.yml" \
+    -e "@secrets/prod.yml" \
+    --ask-vault-pass \
+    playbook.yml
